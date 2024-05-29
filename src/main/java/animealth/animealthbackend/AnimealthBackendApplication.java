8beced91c982d8619655a081d5fec5ee1c12e4f4
hwiju yeom <yeom456@gmail.com>
2024-05-29 23:39:01 +0900

    first commit

---
+++ src/main/java/animealth/animealthbackend/AnimealthBackendApplication.java
@@ -0,0 +1,13 @@
+package animealth.animealthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimealthBackendApplication.class, args);
+	}
+
+}
