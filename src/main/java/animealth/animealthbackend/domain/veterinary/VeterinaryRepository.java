0f54520e3e95395c56603ccc81c7ea98dc020bb7
hwiju yeom <yeom456@gmail.com>
2024-06-21 18:06:56 +0900

    Update VeterinaryRepository.java

--- src/main/java/animealth/animealthbackend/domain/veterinary/VeterinaryRepository.java
+++ src/main/java/animealth/animealthbackend/domain/veterinary/VeterinaryRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VeterinaryRepository extends JpaRepository<Veterinary, Long> {
 
     Veterinary save(Veterinary veterinary);
-    Veterinary findByVeterinaryId(Long veterinaryId);
+    Optional<Veterinary> findByVeterinaryId(Long veterinaryId);
     List<Veterinary> findByVeterinaryName(String veterinaryName);
     List<Veterinary> findAll();
 }
