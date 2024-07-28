ecd39b7dd5a53d7a06a820eb4e8c64dfb18e59e2
hwiju yeom <yeom456@gmail.com>
2024-07-28 23:54:05 +0900

    FEAT: 병원 기록 조회와 관련된 서비스 로직 작성

---
+++ src/main/java/animealth/animealthbackend/api/medical_history/service/MedicalHistoryServiceImpl.java
@@ -0,0 +1,35 @@
+package animealth.animealthbackend.api.medical_history.service;
+
+import animealth.animealthbackend.api.common.exception.NotFoundMedicalHistoryException;
+import animealth.animealthbackend.api.medical_history.dto.MedicalHistoryDTO;
+import animealth.animealthbackend.api.medical_history.dto.MedicalHistoryDTO.MedicalHistoryResponseDTO;
+import animealth.animealthbackend.domain.medical_history.MedicalHistory;
+import animealth.animealthbackend.domain.medical_history.MedicalHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MedicalHistoryServiceImpl implements MedicalHistoryService {
+
+    private final MedicalHistoryRepository medicalHistoryRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<MedicalHistoryResponseDTO> findMedicalHistoryByPetId(Long petId) {
+        List<MedicalHistory> list = medicalHistoryRepository.findByPetId(petId);
+
+        if (list==null && list.isEmpty()){
+            throw new NotFoundMedicalHistoryException("Medical history not found for the given pet ID");
+        }
+
+        return MedicalHistoryResponseDTO.makeDtoListFromEntityList(list);
+    }
+
+
+}
