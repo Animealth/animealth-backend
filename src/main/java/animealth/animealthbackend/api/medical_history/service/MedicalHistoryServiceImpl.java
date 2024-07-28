d28a0be95203db110f7c9a3211bdde49473eb3c5
hwiju yeom <yeom456@gmail.com>
2024-07-28 23:59:46 +0900

    RENAME: 서비스 애너테이션 추가

--- src/main/java/animealth/animealthbackend/api/medical_history/service/MedicalHistoryServiceImpl.java
+++ src/main/java/animealth/animealthbackend/api/medical_history/service/MedicalHistoryServiceImpl.java
@@ -6,6 +6,7 @@ import animealth.animealthbackend.api.medical_history.dto.MedicalHistoryDTO.Medi
 import animealth.animealthbackend.domain.medical_history.MedicalHistory;
 import animealth.animealthbackend.domain.medical_history.MedicalHistoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Service
 public class MedicalHistoryServiceImpl implements MedicalHistoryService {
 
     private final MedicalHistoryRepository medicalHistoryRepository;
